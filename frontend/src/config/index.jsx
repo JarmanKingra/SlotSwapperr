9945d5d2d09dde2c7b1c7cca256a210350031d75
Jarman Jot Singh <jarmankingra63@gmail.com>
2025-11-06 22:09:14 +0530

    Update BASE_URL to production server

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3001/";
+export const BASE_URL = "https://slotswapperr-ox2y.onrender.com/";
 
 const clientServer = axios.create({
     baseURL: BASE_URL
